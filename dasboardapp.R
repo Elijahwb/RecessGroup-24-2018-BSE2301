5180465245bf32f17060ec30754c1a68e2d4b9d9
Elijahbw <brandonelijah099@gmail.com>
2018-07-07 19:56:02 +0300

    Signed-off-by: Wavamuno Brandon Elijah <brandonelijah099@gmail.com>

--- dasboardapp.R
+++ dasboardapp.R
@@ -5,8 +5,8 @@ ui <-dashboardPage(skin = "green",
   dashboardHeader(title = "Recess-Group-24 System Functions", titleWidth = 450,
     dropdownMenu(type = "messages",
                  messageItem(from = "Recess-Group-24",message="Thank you for logging into our system."),
-                 messageItem(from = "New user",message="how do you register?",icon = icon("question"),time = "12:00"),
-                 messageItem(from = "Support",message="The new Server is ready",icon = icon("life-ring"),time = "2018-07-07")
+                 messageItem(from = "New user",message="No registration is needed.",icon = icon("thumbs-up"),time = "12:00"),
+                 messageItem(from = "Support",message="The new Server is ready",icon = icon("server"),time = "2018-07-07")
                  ),#end of messages drop down
     dropdownMenu(type = "tasks",
                  badgeStatus = "success",
@@ -15,7 +15,15 @@ ui <-dashboardPage(skin = "green",
                  taskItem(value = 80, color = "red","Overall project")
                 )#end of tasks drop down
   ),
-  dashboardSidebar(),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard",tabName ="myDashboard",icon=icon("dashboard")),
+      menuItem("Sentiment Analysis",tabName ="sentiment",icon=icon("comment")),
+      menuItem("Word Clouds",tabName ="wordclouds",icon=icon("cloud")),
+      menuItem("Charts",tabName ="charts",icon=icon("bar-chart-o")),
+      menuItem("Characters in the movie",tabName ="characters",icon=icon("users"))
+    )#end of sidebar menu
+  ),
   #begining of the body of the application
   dashboardBody(
     h1("Welcome to the ",strong("Recess Group 24")," System!")
