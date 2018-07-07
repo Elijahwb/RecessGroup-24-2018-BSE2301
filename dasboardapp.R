edd6a2b7a9a5e9b910867706f8d3590b6b09ff95
Elijahbw <brandonelijah099@gmail.com>
2018-07-07 19:28:40 +0300

    Signed-off-by: Elijahbw <brandonelijah099@gmail.com>

--- dasboardapp.R
+++ dasboardapp.R
@@ -6,11 +6,22 @@ ui <-dashboardPage(skin = "green",
     dropdownMenu(type = "messages",
                  messageItem(from = "Recess-Group-24",message="Thank you for logging into our system."),
                  messageItem(from = "New user",message="how do you register?",icon = icon("question"),time = "12:00"),
-                 messageItem(from = "Support",message="The new Server is ready",icon = icon("life-ring"),time = "2018-07-07"))
+                 messageItem(from = "Support",message="The new Server is ready",icon = icon("life-ring"),time = "2018-07-07")
+                 ),#end of messages drop down
+    dropdownMenu(type = "tasks",
+                 badgeStatus = "success",
+                 taskItem(value = 85, color = "green","Documentation of the Project"),
+                 taskItem(value = 75, color = "yellow", "Project deployment"),
+                 taskItem(value = 80, color = "red","Overall project")
+                )#end of tasks drop down
   ),
   dashboardSidebar(),
-  dashboardBody()
+  #begining of the body of the application
+  dashboardBody(
+    h1("Welcome to the ",strong("Recess Group 24")," System!")
+  )#end of the body of the application
 )
+#end of the user interface
 
 server <-function(input, output){}
 
