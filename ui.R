e0f2d2b8782afb598f700f894a3e030b0437550b
Elijahbw <brandonelijah099@gmail.com>
2018-07-09 19:50:13 +0300

    Signed-off-by: Wavamuno Brandon Elijah <brandonelijah099@gmail.com>

--- ui.R
+++ ui.R
@@ -39,19 +39,23 @@ shinyUI(dashboardPage(skin = "yellow",
                      #logo{margin-left:80px;position:fixed;margin-top:170px;}
                       #logo2{margin-left:340px;top:-105px;position:relative;}
                       #logo:hover{transform:scale(1.03);}
-                      #dashboard{margin-left:230px;color:#343031}
+                      #header1{margin-left:230px;color:#343031;margin-top:-70px;position:relative;}
+                      #header2{margin-left:330px;color:#343031;margin-top:-50px;position:relative;}
+                      #header3{margin-left:330px;color:#343031;margin-top:-50px;position:relative;}
+                      #header4{margin-left:330px;color:#343031;margin-top:-50px;position:relative;}
+                      #header5{margin-left:330px;color:#343031;margin-top:-50px;position:relative;}
                       #line{color:black;width:600px;}
                       tr{padding:50px;}
-                    #black-box{margin-left:220px;width:1200px;}
+                    #black-box{margin-left:220px;width:1200px;margin-top:-10px;position:relative;}
                      #cloud-output{border-radius:5px;}
                      #gpIcon{margin-left:900px;margin-top:-42px;}')),
     #************************************************end of the styles sheet**************************************************
               tags$script(src="projectjs1.js")),
-    tags$img(id="gpIcon",src="logo2-4.png"),
+    tags$img(id="gpIcon",src="logo2-6.png"),
     tabItems(
       tabItem(tabName="myDashboard",
               img(" ",src="starwars3.png",id="logo2"),
-              h1(id="dashboard","Welcome to ",strong("Recess-Group-24")," project"),
+              h1(id="header1","Welcome to ",strong("Recess-Group-24")," project"),
               tags$div(id="black-box",box(title="Group-24 Software Engineer Members!",height=250,status="warning",
                   tableOutput("table1"))),
               verbatimTextOutput(outputId = "first")
@@ -59,7 +63,7 @@ shinyUI(dashboardPage(skin = "yellow",
     
     
     tabItem(tabName="sentiments",
-            h1(id="dashboard","Sentiment Panel"),
+            h1(id="header2","Sentiment Panel"),
             tabBox(id="tab1",title="Negative/Positive",width = "600",
                    tabPanel("Episode IV",plotOutput("sentiment1")),
                    tabPanel("Episode V"),
@@ -68,7 +72,7 @@ shinyUI(dashboardPage(skin = "yellow",
     
     
     tabItem(tabName="highfrequency",icon=icon("cloud"),
-            h1(id="dashboard","Wordcloud Panel"),
+            h1(id="header3","Wordcloud Panel"),
             tabBox(id="tab2",title="Frequency of words",width = "600",
                    tabPanel("Episode IV",height="200px",
                             tags$div(id="cloud-output",
@@ -89,7 +93,7 @@ shinyUI(dashboardPage(skin = "yellow",
                                          wordcloud2Output(outputId="cloud3"),width=300)
                             ))),br(),br(),br(),br()),
     tabItem(tabName="lowfrequency",icon=icon("cloud"),
-            h1(id="dashboard","Wordcloud Panel"),
+            h1(id="header3","Wordcloud Panel"),
             tabBox(id="tab2",title="Frequency of words",width = "600",
                    tabPanel("Episode IV",height="200px",
                             tags$div(id="cloud-output",
@@ -109,33 +113,33 @@ shinyUI(dashboardPage(skin = "yellow",
     #******************************************end of word clouds sub-items***************************************************
     
     tabItem(tabName="chartepisode4",
-            h1(id="dashboard","Charts Panel"),
+            h1(id="header4","Charts Panel"),
             box("chart-1",status="warning",plotlyOutput("chart1"),width="600",height="500",background = "yellow")
     ),tabItem(tabName="chartepisode5",
-              h1(id="dashboard","Charts Panel"),
+              h1(id="header4","Charts Panel"),
               box("chart-1",status="warning",plotlyOutput("chart2"),width="600",height="500",background = "yellow")
     ),tabItem(tabName="chartepisode6",
-              h1(id="dashboard","Charts Panel"),
+              h1(id="header4","Charts Panel"),
               box("chart-1",status="warning",plotlyOutput("chart3"),width="600",height="500",background = "yellow")
     ),#*************************************end of dialogue charts sub-items**************************************************
     
     
     tabItem(tabName="cepisode4",
-            h1(id="dashboard","Characters' Panel"),
+            h1(id="header5","Characters' Panel"),
             box("Number of Characters",status="danger",verbatimTextOutput("characters4"),
                 h2("Character names"),br(),
                 verbatimTextOutput("characterNames4"),
                 verbatimTextOutput("information4"),background="yellow")
     ),
     tabItem(tabName="cepisode5",
-            h1(id="dashboard","Characters' Panel"),
+            h1(id="header5","Characters' Panel"),
             box("Number of Characters",status="danger",verbatimTextOutput("characters5"),
                 h2("Character names"),br(),
                 verbatimTextOutput("characterNames5"),
                 verbatimTextOutput("information5"),background="yellow")
     ),
     tabItem(tabName="cepisode6",
-            h1(id="dashboard","Characters' Panel"),
+            h1(id="header5","Characters' Panel"),
             box("Number of Characters",status="danger",verbatimTextOutput("characters6"),
                 h2("Character names"),br(),
                 verbatimTextOutput("characterNames6"),
